e0e199024e76db87fc89bcfdb532751ac69a6a8b
DanYellow <jeanlouis.danielo@sfr.fr>
2020-01-07 17:44:08 +0100

    Fix docker issue

--- test-hook.sh
+++ test-hook.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "Hello World"
+echo "----------- $ARGUMENT"
+
+if [[ $ARGUMENT =~ .+\.[jpg|JPG] ]]
+    then
+    # Then next few lines are optional
+    # Alternatively pass $ARGUMENT directly to your image viewer eg:-
+    # /usr/bin/shotwell "$ARGUMENT" &
+    DIRNAME=$(dirname "$ARGUMENT")
+    BASENAME=$(basename "$ARGUMENT")
+    NEWFILENAME="$DIRNAME/flask-app/_tmp/$BASENAME"
+
+    sudo mv "$ARGUMENT" "$NEWFILENAME"    
+fi
